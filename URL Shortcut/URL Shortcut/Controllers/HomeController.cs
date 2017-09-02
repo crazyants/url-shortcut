9b78aa54bbb316ae84fadf14523c2f7c3c4c41c5
kamyar-nemati <kamyarnemati@gmail.com>
2017-09-03 06:25:00 +0800

    Project cleaned up. Homepage set. Cassandra driver installed.

---
+++ URL Shortcut/URL Shortcut/Controllers/HomeController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace URL_Shortcut.Controllers
+{
+    [Route("")]
+    [Route("Home")]
+    public class HomeController : Controller
+    {
+        [Route("")]
+        [Route("Index")]
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
