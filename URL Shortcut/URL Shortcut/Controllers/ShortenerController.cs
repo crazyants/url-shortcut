b35d21e27b441c119c10128d50a6646cddfbca9e
kamyar-nemati <kamyarnemati@gmail.com>
2017-09-03 15:44:25 +0800

    CassandraConnect class implemented. Database settings json added.

---
+++ URL Shortcut/URL Shortcut/Controllers/ShortenerController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace URL_Shortcut.Controllers
+{
+    [Route("api/shortener")]
+    public class ShortenerController : Controller
+    {
+        [Route("")]
+        [HttpPost]
+        public string Index([FromHeader]string url)
+        {
+            Utils.CassandraConnect cassandraConnect = new Utils.CassandraConnect();
+            
+            return url;
+        }
+    }
+}
---
+++ URL Shortcut/URL Shortcut/Utils/CassandraConnect.cs
@@ -0,0 +1,59 @@
+﻿using Microsoft.Extensions.Configuration;
+using System.IO;
+using Cassandra;
+
+namespace URL_Shortcut.Utils
+{
+    public class CassandraConnect
+    {
+        private IConfigurationRoot configuration;
+
+        private Cluster cluster;
+        private ISession session;
+
+        public CassandraConnect()
+        {
+            this.configuration = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("dbsettings.json")
+                .Build();
+
+            this.cluster = null;
+            this.session = null;
+
+            this.Connect();
+        }
+
+        private ISession Connect()
+        {
+            string host = this.configuration["Cassandra:Host"];
+            string port = this.configuration["Cassandra:Port"];
+            string username = this.configuration["Cassandra:Username"];
+            string password = this.configuration["Cassandra:Password"];
+            string database = this.configuration["Cassandra:Database"];
+
+            this.cluster = Cluster
+                .Builder()
+                .AddContactPoint(host)
+                .WithPort(int.Parse(port))
+                .WithCredentials(username, password)
+                .WithDefaultKeyspace(database)
+                .WithQueryOptions(new QueryOptions().SetConsistencyLevel(ConsistencyLevel.All))
+                .WithLoadBalancingPolicy(new RoundRobinPolicy())
+                .WithCompression(CompressionType.Snappy)
+                .Build();
+
+            return null;
+        }
+
+        public ISession GetSession()
+        {
+            if (this.session == null)
+            {
+                this.Connect();
+            }
+
+            return this.session;
+        }
+    }
+}
