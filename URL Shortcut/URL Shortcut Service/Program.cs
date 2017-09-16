44be38049d7a942d633d6981ffa336530af79e19
kamyar-nemati <kamyarnemati@gmail.com>
2017-09-16 13:47:42 +0800

    New project created to handle background services.

---
+++ URL Shortcut/URL Shortcut Service/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceProcess;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace URL_Shortcut_Service
+{
+    static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        static void Main()
+        {
+#if DEBUG
+            ListenerService listenerService;
+            listenerService = new ListenerService();
+            listenerService.GoDebug(null);
+#else
+            ServiceBase[] ServicesToRun;
+            ServicesToRun = new ServiceBase[]
+            {
+                new ListenerService()
+            };
+            ServiceBase.Run(ServicesToRun);
+#endif
+        }
+    }
+}
